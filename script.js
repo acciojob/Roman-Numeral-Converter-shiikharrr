3893f53383b1c8f86e0f05334bbc258cc9c82c4d
shiikharrr <69209995+shiikharrr@users.noreply.github.com>
2026-01-04 13:58:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-
-	const M = obj[0][0], D = obj[1][0], C = obj[2][0],  L = obj[3][0], X = obj[4][0], V = obj[5][0], I = [6][0];
-
-	const map = [[M, 1000], [C + M, 9000], [D, 500], [C + D, 400], [C, 100], [X + C, 90], [L, 50], [X + L, 40], [X, 10], [I + X, 9], [V, 5], [I + V, 4], [I, 1],];
-
-	let res = '';
-	for (const [sym, val] of map){
-		while (num >= val){
-			res += sym;
-			num -= val;
-		}
-	}
-	return res;
+  	const obj = [
+     ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+    ];
 
   //your code here
-
+	let result="";
+	for(const [roman,value] of obj){
+          while(num>=value){
+              result+=roman;
+			  num=num-value;	  
+		  }
+	}
+	return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
 
+console.log(convertToRoman(36));
+alert(convertToRoman(36));
 
 
 
